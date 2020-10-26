8c55df635234c078212ba9fe3b1e3e03f8d4f775
Jack <tran0165@Algonquinlive.com>
2020-10-26 15:23:16 -0400

    quick css completed

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyAfe1OnNj-Zmq-BOzvARfAbzMRsZwD8m4Y";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
